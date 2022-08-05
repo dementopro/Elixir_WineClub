afff048492dfd7264ce76eac7b8e59a90586ba1a
dementopro <dementopro47@gmail.com>
2022-08-05 12:21:30 -0700

    change to endpoint config for ssl

--- config/prod.exs
+++ config/prod.exs
@@ -14,10 +14,10 @@ import Config
 #config :mwcweb, MwcwebWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 config :mwcweb, MwcwebWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "fix.modernwineclub.com", port: 443],
+  url: [scheme: "https", host: "c7test.modernwineclub.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  check_origin: ["https://fux.modernwineclub.com","https://mwc-wine-blog.herokuapp.com" ]
+  check_origin: ["https://c7test.modernwineclub.com","https://mwcweb.fly.dev" ]
 
 
 # Do not print debug messages in production
