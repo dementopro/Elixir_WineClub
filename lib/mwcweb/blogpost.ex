079cecd9bb9df6ebebb59899069ae2a39634a889
dementopro <dementopro47@gmail.com>
2022-09-06 21:56:00 -0700

    more iamges fro blog post

--- lib/mwcweb/blogpost.ex
+++ lib/mwcweb/blogpost.ex
@@ -11,10 +11,14 @@ defmodule Mwcweb.Blogpost do
           summary: String.t(),
           image: String.t(),
           content: String.t(),
+          image1: String.t(),
+          image2: String.t(),
+          image3: String.t(),
+          image4: String.t(),
           blogcover_image: String.t(),
          # author: String.t(),
           published_at: Date.t()
         }
 
-  defstruct [:id, :bslug, :title, :disporder, :description, :summary, :image, :blogcover_image, :content, :published_at]
+  defstruct [:id, :bslug, :title, :disporder, :description, :summary, :image, :image1, :image2, :image3, :image4, :blogcover_image, :content, :published_at]
 end
